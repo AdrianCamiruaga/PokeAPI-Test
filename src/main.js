9316eb6dd68ec7dbd24ea2c74fda1a0670b73fc1
SergioCamiruaga <sergio_camiruaga@hotmail.com>
2018-07-03 18:19:57 -0500

    Se hicieron cambios para implementar el consumo de la API y obtener pokemones

--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
 import Vue from 'vue'
-import App from './App.vue'
+import axios from 'axios';
 
 new Vue({
   el: '#app',
   data:{
         pokemones: ["Charizar", "Bulbasur", "Pikachu"],
 
+  },
+  mounted(){
+
+        axios.get('https://pokeapi.co/api/v2/pokemon-form/').then(response =>
+        {
+          this.pokemones = response.data.results;
+        });
   }
   
 })
