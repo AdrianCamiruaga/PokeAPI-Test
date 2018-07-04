d31ba087a925de608f7c12c478384c6e5a053be3
SergioCamiruaga <sergio_camiruaga@hotmail.com>
2018-07-04 12:05:10 -0500

    Se agrego cuadro que muestra imagen y nombre del pokemon seleccionado y algunos estilos para el mismo cuadro de informacion

--- src/main.js
+++ src/main.js
@@ -4,23 +4,42 @@ import axios from 'axios';
 new Vue({
   el: '#app',
   data:{
-        pokemones: []
+        pokemones: [],
+        infoPokemon: [],
+        srcImg: '',
+        nombrePokemon: '',
+        habilidadesPokemon: '',
+        estadisticasPokemon: '',
+        pesoPokemon: '',
+        alturaPokemon: '',
+        tipoPokemon: '',
+        experienciaBase: '',
+        cuadroInfoPokemon: false
   },
   methods:{
-    numRandom : function(){
-      return Math.floor(Math.random() * (300 - 70) + 1);
+    numRandom : function()
+    {
+      return Math.floor(Math.random() * (300 - 1) + 1);
+    },
+    clickInfoPokemon: function(event)
+    {
+      let url = 'https://pokeapi.co/api/v2/pokemon-form/'+event.toElement.innerText.trim();
+      axios.get(url).then(response =>
+      {
+        this.srcImg = response.data.sprites.front_default;
+        this.infoPokemon = response.data;
+        this.nombrePokemon = "<h3>"+this.infoPokemon.name+"</h3>";
+        //console.log(response.data);
+      });
+      this.cuadroInfoPokemon = true;
     }
   },
   mounted(){
-        //axios.get('https://pokeapi.co/api/v2/pokemon-form/').then(response =>
-
           let url = 'https://pokeapi.co/api/v2/pokemon-form/?limit=20&offset='+this.numRandom();
           axios.get(url).then(response =>
           {
+            //console.log(response.status); 
             this.pokemones = response.data.results;
           });
-        
-
   }
-  
 })
